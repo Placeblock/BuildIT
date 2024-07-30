18e45073ccc49a196da488f33b693815050482dd
felix <felix@weglehner.info>
2024-07-31 00:40:15 +0200

    First tests using raylib

--- src/graphics/graphics.cpp
+++ src/graphics/graphics.cpp
@@ -5,12 +5,22 @@
 #include "graphics.h"
 
 #include <raylib.h>
+#include <iostream>
 
 void Graphics::start() {
     const int screenWidth = 800;
     const int screenHeight = 450;
 
     InitWindow(screenWidth, screenHeight, "raylib test");
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        EndDrawing();
+        std::cout << GetFPS() << "\n";
+    }
 
     CloseWindow();
 }
