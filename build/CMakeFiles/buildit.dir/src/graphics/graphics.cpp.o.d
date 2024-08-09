9b3c2d04867e2acf8b35fb43ab07b62718b13006
felix <felix@weglehner.info>
2024-08-09 02:39:47 +0200

    Removed a whole lot of crap LOL

--- build/CMakeFiles/buildit.dir/src/graphics/graphics.cpp.o.d
+++
@@ -1,6 +0,0 @@
-CMakeFiles/buildit.dir/src/graphics/graphics.cpp.o: \
- /home/felix/Dokumente/development/c/buildit/src/graphics/graphics.cpp \
- /usr/include/stdc-predef.h \
- /home/felix/Dokumente/development/c/buildit/src/graphics/graphics.h \
- /usr/local/include/raylib.h \
- /usr/lib/gcc/x86_64-linux-gnu/12/include/stdarg.h
--- build/CMakeFiles/buildit.dir/src/math/point.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/buildit.dir/src/math/point.cpp.o: \
- /home/felix/Dokumente/development/c/buildit/src/math/point.cpp \
- /usr/include/stdc-predef.h \
- /home/felix/Dokumente/development/c/buildit/src/math/point.h
