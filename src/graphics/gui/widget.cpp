2519a0ddb757f203e0ba406a6a0c8baf63ec3304
felix <felix@weglehner.info>
2024-08-14 17:13:21 +0200

    Working on Buffer management of GUI

--- src/graphics/gui/widget.cpp
+++ src/graphics/gui/widget.cpp
@@ -6,33 +6,52 @@
 #include "widget.h"
 
 void Widget::setBufferSize(uint newBufferSize) {
-    int delta = newBufferSize - this->bufferSize;
+    int delta = int(newBufferSize) - int(this->bufferSize);
     this->bufferSize = newBufferSize;
     this->parent->updateBufferSizeRecursive(this, delta);
 }
 
 void Widget::updateBufferSizeRecursive(Widget* child, int delta) {
-    uint newBufferSize = 0;
-    for (const auto &item: this->children) {
-        newBufferSize += item->bufferSize;
+    this->childrenBufferSize += delta;
+
+    auto iter = std::find_if(this->children.begin(), this->children.end(), [&](const std::unique_ptr<Widget>& w) {
+        return child == w.get();
+    });
+    while (++iter != this->children.end()) {
+        (*iter)->moveBufferIndexRecursive(delta);
     }
-    this->bufferSize = newBufferSize;
+
     if (this->parent != nullptr) {
         this->parent->updateBufferSizeRecursive(this, delta);
-    } else {
-        auto iter = std::find_if(this->children.begin(), this->children.end(), [&](const std::unique_ptr<Widget>& w) {
-            return child == w.get();
-        });
-        iter = std::next(iter);
-        while (iter != this->children.end()) {
-            iter->get()->moveBufferIndexRecursive(delta);
-        }
     }
 }
 
 void Widget::moveBufferIndexRecursive(int delta) {
     this->bufferIndex += delta;
-    for (const auto &item: this->children) {
-        item->moveBufferIndexRecursive(delta);
+    for (const auto &child: this->children) {
+        child->moveBufferIndexRecursive(delta);
     }
+}
+
+void Widget::addChild(std::unique_ptr<Widget>& child) {
+    if (this->children.empty()) {
+        child->setBufferIndex(this->bufferIndex + this->bufferSize);
+    } else {
+        child->setBufferIndex(this->children.back()->bufferIndex + this->children.back()->getRequiredBufferSpace());
+    }
+    this->childrenBufferSize += child->getRequiredBufferSpace();
+    this->parent->updateBufferSizeRecursive(this, int(child->getRequiredBufferSpace()));
+    this->children.push_back(std::move(child));
+}
+
+void Widget::removeChild(Widget *child) {
+    auto iter = std::find_if(this->children.begin(), this->children.end(), [&child](const std::unique_ptr<Widget>& c){
+        return c.get() == child;
+    });
+    this->updateBufferSizeRecursive(iter->get(), -int(child->getRequiredBufferSpace()));
+    this->children.erase(iter);
+}
+
+void Widget::setBufferIndex(uint index) {
+    this->bufferIndex = index;
 }
--- src/graphics/gui/widget.h
+++ src/graphics/gui/widget.h
@@ -9,11 +9,28 @@
 #include <list>
 #include <memory>
 #include "glm/vec2.hpp"
+#include "graphics/types.h"
+
+enum class Action {};
+
+class Widget;
+
+class GUI {
+private:
+    std::unique_ptr<Widget> root;
+};
 
 class Widget {
-    glm::vec2 size;
-    uint bufferIndex;
-    uint bufferSize;
+private:
+    GUI* gui;
+
+    intVec2 size;
+    uint bufferIndex = 0;
+    uint bufferSize = 0;
+    uint childrenBufferSize;
+    [[nodiscard]] uint getRequiredBufferSpace() const {
+        return this->bufferSize + this->childrenBufferSize;
+    }
 
     Widget* parent;
     std::list<std::unique_ptr<Widget>> children;
@@ -21,8 +38,21 @@ class Widget {
     void setBufferSize(uint newBufferSize); // Calls updateBufferSizeRecursive
     void updateBufferSizeRecursive(Widget* widget, int delta);
     void moveBufferIndexRecursive(int delta);
+
+    void addChild(std::unique_ptr<Widget>& child);
+    void removeChild(Widget* child);
 public:
-    ~Widget() {
+    Widget(GUI* gui, intVec2 size, Widget* parent = nullptr) : gui(gui), size(size), parent(parent) {};
+
+    void setBufferIndex(uint index);
+
+    virtual uint calcBufferSize() = 0;
+    virtual void onMouseOver() = 0;
+    virtual void onMouseOut() = 0;
+    virtual void onMouseAction(intVec2 relPos, int button, int mouseAction) = 0;
+    virtual void render(std::list<float>& vertices, std::list<float>& texCoords, std::list<Color> colors, std::list<uint> texture) = 0;
+
+    virtual ~Widget() {
         for (const auto &child: this->children) {
             child->parent = nullptr;
         }
