9dcd1f0ae3f2067c9f38479fe122c6058c88d107
felix <felix@weglehner.info>
2024-09-11 00:29:46 +0200

    Fixing runtime errors

--- src/graphics/kit/kit.cpp
+++ src/graphics/kit/kit.cpp
@@ -5,7 +5,6 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include "kit.h"
-#include "graphics/shapes/shapes.h"
 #include "image/stb_image.h"
 #include "graphics/gui/widgets/text.h"
 #include "graphics/circuitBoard/history/actions/createComponentAction.h"
