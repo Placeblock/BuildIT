e32f3b9cc078c8b65688fb2fa41cde951e2b69c1
felix <felix@weglehner.info>
2024-09-10 02:00:17 +0200

    Working on component rendering - to make better circuit board abstraction (The second part of the sentence is definitely not only there to have a reason to make these changes in a branch about a completely different topic yeah yeah)

---
+++ src/graphics/circuitBoard/components/renderer/componentRenderers.cpp
@@ -0,0 +1,5 @@
+//
+// Created by felix on 9/10/24.
+//
+
+#include "componentRenderers.h"
---
+++ src/graphics/circuitBoard/components/renderer/componentRenderers.h
@@ -0,0 +1,18 @@
+//
+// Created by felix on 9/10/24.
+//
+
+#ifndef BUILDIT_COMPONENTRENDERERS_H
+#define BUILDIT_COMPONENTRENDERERS_H
+
+
+#include "cablingRenderer.h"
+
+class ComponentRenderers {
+
+    CablingRenderer *cablingRenderer;
+
+};
+
+
+#endif //BUILDIT_COMPONENTRENDERERS_H
