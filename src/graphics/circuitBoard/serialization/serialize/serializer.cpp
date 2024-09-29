35be4e647889c7689077b90c7ece8e482642092a
felix <felix@weglehner.info>
2024-09-29 03:51:07 +0200

    Better serialization

--- src/graphics/circuitBoard/serialization/serialize/serializer.cpp
+++ src/graphics/circuitBoard/serialization/serialize/serializer.cpp
@@ -7,19 +7,19 @@
 
 void Serializer::serialize(std::unordered_set<std::shared_ptr<Component>>* components,
                            std::queue<Sim::Node*> updateQueue) {
+    const uint32_t componentSize = components->size(); // Type for simulation queue
+    this->context.serialized.write((const char*)&componentSize, 4); // Component type
     for (const auto &component: *components) {
         uint16_t id = this->keyIDs.getID(component->getUniqueID());
         this->context.serialized.write((const char*)&id, 2);
         component->serialize(this->context);
     }
-    constexpr uint16_t type = 3; // Type for simulation queue
-    this->context.serialized.write((const char*)&type, 2); // Component type
     const uint32_t queueSize = updateQueue.size();
-    this->context.serialized.write((const char*)&queueSize, sizeof(queueSize)); // Component type
+    this->context.serialized.write((const char*)&queueSize, 4); // Component type
     while (!updateQueue.empty()) {
         Sim::Node *node = updateQueue.front();
         const uint32_t id = this->context.nodeIDs.getID(node);
-        this->context.serialized.write((const char*)&id, sizeof(id)); // Component type
+        this->context.serialized.write((const char*)&id, 4); // Component type
         updateQueue.pop();
     }
     uint16_t componentTypes = this->keyIDs.ids.size();
