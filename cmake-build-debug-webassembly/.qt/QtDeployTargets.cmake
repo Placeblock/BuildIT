3d0a6108bc74d410ccf8fcb09edceb05cfd186da
felix <felix@weglehner.info>
2025-03-25 21:23:37 +0100

    Removed unnecessary stuff

--- cmake-build-debug-webassembly/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_BuildIT_FILE /home/felix/Dokumente/development/c/BuildIT/cmake-build-debug-webassembly/src/app/BuildIT.js)
-set(__QT_DEPLOY_TARGET_BuildIT_TYPE EXECUTABLE)
--- cmake-build-debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_BuildIT_FILE /home/felix/Dokumente/development/c/BuildIT/cmake-build-debug/src/app/BuildIT)
-set(__QT_DEPLOY_TARGET_BuildIT_TYPE EXECUTABLE)
