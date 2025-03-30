4ebc6ef05cb31a2d5814799679a1a86a64b995aa
felix <felix@weglehner.info>
2025-03-30 23:23:34 +0200

    Working on Everything at the same time because I'M A SUPERHERO RARR

---
+++ src/events/event.cpp
@@ -0,0 +1,5 @@
+//
+// Created by felix on 30.03.25.
+//
+
+#include "Event.h"
---
+++ src/events/event.h
@@ -0,0 +1,18 @@
+//
+// Created by felix on 30.03.25.
+//
+
+#ifndef EVENT_H
+#define EVENT_H
+
+
+
+struct Event {
+
+};
+
+struct 
+
+
+
+#endif //EVENT_H
